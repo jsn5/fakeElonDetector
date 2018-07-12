19763aa4a56e1514a15eae01b8cad29c59cdf0aa
Jaison Saji <jaisonsaji@gmail.com>
2018-07-13 00:30:39 +0530

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -2,13 +2,14 @@ import tweepy
 from credentials import *
 import editdistance
 import time
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-
-
 c = tweepy.Cursor(api.search,q='to:'+'elonmusk',since_id='1017463575919693826',include_entities=True).items()
 
+real_screen_name = 'elonmusk'
+real_name = 'Elon Musk'
 
 while True:
 	try:
@@ -18,9 +19,9 @@ while True:
 		screen_name = tweet.user.screen_name
 		tweet_id = tweet.id
 		print(tweet_id)
-		if screen_name != 'elonmusk':
-			name_dist = editdistance.eval('Elon Musk', name)
-			screen_dist = editdistance.eval('elonmusk',screen_name)
+		if screen_name != real_screen_name:
+			name_dist = editdistance.eval(real_name, name)
+			screen_dist = editdistance.eval(real_screen_name',screen_name)
 			if name_dist <= 2 or screen_dist <= 4:
 				print("fake found")
 				api.update_status("*❗️❗️beep boop❗️❗️* Fake Elon Musk detected❎, report as spam ❎ @elonmusk @{}".format(screen_name), in_reply_to_status_id=tweet_id)
